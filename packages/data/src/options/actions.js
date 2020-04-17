3e44a7d5249cdadd0efa76d269d706b876969360
Paul Sealock <psealock@gmail.com>
2020-04-17 18:16:37 +1200

    updateOptions

--- packages/data/src/options/actions.js
+++ packages/data/src/options/actions.js
@@ -1,13 +1,20 @@
 /**
+ * External Dependencies
+ */
+
+import { apiFetch } from '@wordpress/data-controls';
+
+/**
  * Internal Dependencies
  */
 import TYPES from './action-types';
+import { WC_ADMIN_NAMESPACE } from '../constants';
 
 export function recieveOptions( options ) {
-    return {
-        type: TYPES.RECIEVE_OPTIONS,
-        options,
-    };
+	return {
+		type: TYPES.RECIEVE_OPTIONS,
+		options,
+	};
 }
 
 export function setIsRequesting( isRequesting ) {
@@ -22,4 +29,23 @@ export function setError( error ) {
 		type: TYPES.SET_ERROR,
 		error,
 	};
+}
+
+export function* updateOptions( data ) {
+	yield setIsRequesting( true );
+
+	try {
+		const results = yield apiFetch( {
+			path: WC_ADMIN_NAMESPACE + '/options',
+			method: 'POST',
+			data,
+		} );
+
+		// Add results to state or something and check for error.
+		yield setIsRequesting( false );
+		return results;
+	} catch ( error ) {
+		yield setError( error );
+		return error;
+	}
 }
